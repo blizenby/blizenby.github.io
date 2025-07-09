68562ee7a88b7a35f47093ab7f2ca7aec727ec5c
blizenby <benlisenby@outlook.com>
2025-07-08 20:54:55 -0400

    Update script.js
    v4

--- script.js
+++ script.js
@@ -23,4 +23,15 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+  function showSection(id) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+
+  const selected = document.getElementById(id);
+  if (selected) {
+    selected.style.display = 'block';
+  }
+}
 });
